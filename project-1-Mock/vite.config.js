1156b1f4c6224f416138523aea835cbfc50be614
Zach-O-Bates <zbates486@gmail.com>
2025-03-28 01:58:06 -0400

    testing

--- project-1-Mock/vite.config.js
+++ project-1-Mock/vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/Project1Mock/",
+  base: "https://zach-o-bates.github.io/Project1Mock/",
 });
